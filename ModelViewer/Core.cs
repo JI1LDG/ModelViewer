32fe5ae261e05f9efd7a7c4fb549db36db24b476
Shizave I <shizave@outlook.jp>
2016-09-29 15:43:32 +0900

    5th: テクスチャマッピング・深度バッファ

--- ModelViewer/Core.cs
+++ ModelViewer/Core.cs
@@ -9,11 +9,13 @@ namespace ModelViewer {
 		protected Dx11.Device device;
 		protected Dxgi.SwapChain swapChain;
 		protected Dx11.RenderTargetView renderTarget;
+		protected Dx11.DepthStencilView depthStencil;
 
 		protected virtual void LoadContent() { }
 		protected virtual void UnloadContent() { }
 
 		public void Run() {
+			this.Height = this.Width = 500;
 			InitializeDevice();
 			MessagePump.Run(this, Draw);
 			DisposeDevice();
@@ -42,6 +44,8 @@ namespace ModelViewer {
 			);
 
 			InitializeRenderTarget();
+			InitializeDepthStencil();
+			device.ImmediateContext.OutputMerger.SetTargets(depthStencil, renderTarget);
 			InitializeViewport();
 			LoadContent();
 		}
@@ -53,9 +57,21 @@ namespace ModelViewer {
 			}
 		}
 
+		private void InitializeDepthStencil() {
+			using(var depthBuffer = new Dx11.Texture2D(device,
+				new Dx11.Texture2DDescription() {
+					ArraySize = 1, BindFlags = Dx11.BindFlags.DepthStencil,
+					Format = Dxgi.Format.D32_Float,
+					Width = ClientSize.Width, Height = ClientSize.Height,
+					MipLevels = 1, SampleDescription = new Dxgi.SampleDescription(1, 0)
+				})) {
+				depthStencil = new Dx11.DepthStencilView(device, depthBuffer);
+			}
+		}
+
 		private void InitializeViewport() {
 			device.ImmediateContext.Rasterizer.SetViewports(
-				new Dx11.Viewport() { Width = ClientSize.Width, Height = ClientSize.Height }
+				new Dx11.Viewport() { Width = ClientSize.Width, Height = ClientSize.Height, MaxZ = 1 }
 			);
 		}
 
--- ModelViewer/DrawPmdModel.cs
+++ ModelViewer/DrawPmdModel.cs
@@ -2,6 +2,7 @@
 using SlimDX;
 using SlimDX.D3DCompiler;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Dx11 = SlimDX.Direct3D11;
 using Dxgi = SlimDX.DXGI;
@@ -11,20 +12,25 @@ namespace ModelViewer {
 		Dx11.Effect effect;
 		Dx11.InputLayout vertexLayout;
 		Dx11.Buffer vertexBuffer;
+		Dx11.Buffer indexBuffer;
+		Dx11.ShaderResourceView[] texture;
 		PmdLoader pmdLoader;
 		int flameCount;
+		string parentDir;
 
 		public DrawPmdModel(string Path) {
 			pmdLoader = new PmdLoader(Path);
+			parentDir = System.IO.Path.GetDirectoryName(Environment.CurrentDirectory + "\\" + Path) + "\\";
 			flameCount = 0;
 		}
 
 		protected override void Draw() {
 			device.ImmediateContext.ClearRenderTargetView(renderTarget, new Color4(1, 0, 0, 1));
+			device.ImmediateContext.ClearDepthStencilView(depthStencil, Dx11.DepthStencilClearFlags.Depth, 1, 0);
 
 			UpdateCamera();
 			InitializeInputAsselbler();
-			DrawTriangle();
+			DrawModel();
 
 			swapChain.Present(0, Dxgi.PresentFlags.None);
 
@@ -32,7 +38,7 @@ namespace ModelViewer {
 		}
 
 		private void UpdateCamera() {
-			var world = Matrix.RotationY((flameCount / 20 % 360) * (float)Math.PI / 180);
+			var world = Matrix.RotationY(flameCount / 20 * (float)Math.PI / 180);
 
 			var view = Matrix.LookAtRH(
 				new Vector3(0, 10, -10), new Vector3(0, 10, 0), new Vector3(0, 1, 0)
@@ -50,19 +56,32 @@ namespace ModelViewer {
 		private void InitializeInputAsselbler() {
 			device.ImmediateContext.InputAssembler.InputLayout = vertexLayout;
 			device.ImmediateContext.InputAssembler.SetVertexBuffers(
-				0, new Dx11.VertexBufferBinding(vertexBuffer, sizeof(float) * 3, 0));
+				0, new Dx11.VertexBufferBinding(vertexBuffer, System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertexData)), 0));
+			device.ImmediateContext.InputAssembler.SetIndexBuffer(indexBuffer, Dxgi.Format.R16_UInt, 0);
 			device.ImmediateContext.InputAssembler.PrimitiveTopology = Dx11.PrimitiveTopology.TriangleList;
 		}
 
-		private void DrawTriangle() {
-			effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);
-			device.ImmediateContext.Draw(pmdLoader.Index.Length * 3, 0);
+		private void DrawModel() {
+			int startIdx = 0;
+			for(int i = 0;i < pmdLoader.Material.Length;i++) {
+				if(texture[i] == null) {
+					effect.GetVariableByName("tex").AsScalar().Set(false);
+				} else {
+					effect.GetVariableByName("tex").AsScalar().Set(true);
+					effect.GetVariableByName("normalTexture").AsResource().SetResource(texture[i]);
+				}
+				effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);
+				device.ImmediateContext.DrawIndexed(pmdLoader.Material[i].IndiciesCount, startIdx, 0);
+				startIdx += pmdLoader.Material[i].IndiciesCount;
+			}
 		}
 
 		protected override void LoadContent() {
 			InitializeEffect();
 			InitializeVertexLayout();
 			InitializeVertexBuffer();
+			InitializeIndexBuffer();
+			InitializeTexture();
 			InitializeRasterizerState();
 		}
 
@@ -77,7 +96,11 @@ namespace ModelViewer {
 				device, effect.GetTechniqueByIndex(0).GetPassByIndex(0).Description.Signature,
 				new[] {
 					new Dx11.InputElement() {
-						SemanticName = "SV_Position", Format = Dxgi.Format.R32G32B32_Float
+						SemanticName = "SV_Position", Format = Dxgi.Format.R32G32B32_Float,
+					},
+					new Dx11.InputElement() {
+						SemanticName = "TEXCOORD", Format = Dxgi.Format.R32G32_Float,
+						AlignedByteOffset = Dx11.InputElement.AppendAligned
 					}
 				}
 			);
@@ -85,12 +108,47 @@ namespace ModelViewer {
 
 		private void InitializeVertexBuffer() {
 			using(var vertexStream = new DataStream(
-				pmdLoader.Index.SelectMany(x => x.Indicies).Select(x => pmdLoader.Vertex[x].Position).ToArray(), true, true)) {
+				pmdLoader.Vertex.Select(x => new VertexData() {
+					Position = x.Position, Uv = x.Uv
+				}).ToArray(), true, true)) {
 				vertexBuffer = new Dx11.Buffer(device, vertexStream,
 					new Dx11.BufferDescription() {
-						SizeInBytes = (int)vertexStream.Length, BindFlags = Dx11.BindFlags.VertexBuffer
+						SizeInBytes = (int)vertexStream.Length,
+						BindFlags = Dx11.BindFlags.VertexBuffer,
+						StructureByteStride = sizeof(float),
 					}
 				);
+			}
+		}
+
+		private void InitializeIndexBuffer() {
+			using(var indexStream = new DataStream(pmdLoader.Index.SelectMany(x => x.Indicies).ToArray(), true, true)) {
+				indexBuffer = new Dx11.Buffer(device, indexStream, new Dx11.BufferDescription() {
+					SizeInBytes = (int)indexStream.Length,
+					BindFlags = Dx11.BindFlags.IndexBuffer,
+					StructureByteStride = sizeof(short)
+				});
+			}
+		}
+
+		private void InitializeTexture() {
+			texture = new Dx11.ShaderResourceView[pmdLoader.Material.Length];
+			for(int i = 0;i < texture.Length; i++) {
+				try {
+					texture[i] = Dx11.ShaderResourceView.FromFile(device, parentDir + pmdLoader.Material[i].TextureFileName, 
+						new Dx11.ImageLoadInformation() {
+							Format = Dxgi.Format.R32G32B32A32_Float,
+							FilterFlags = Dx11.FilterFlags.Triangle,
+							MipFilterFlags = Dx11.FilterFlags.Triangle,
+							Usage = Dx11.ResourceUsage.Default,
+							BindFlags = Dx11.BindFlags.ShaderResource,
+							MipLevels = -1
+						}
+					);
+				} catch(Dx11.Direct3D11Exception e) {
+					Console.WriteLine("Texture of Material" + i + " not found");
+					continue;
+				}
 			}
 		}
 
@@ -104,9 +162,16 @@ namespace ModelViewer {
 
 		protected override void UnloadContent() {
 			device.ImmediateContext.Rasterizer.State.Dispose();
+			foreach(var t in texture) t?.Dispose();
 			effect.Dispose();
 			vertexLayout.Dispose();
 			vertexBuffer.Dispose();
+			indexBuffer.Dispose();
 		}
+	}
+
+	struct VertexData {
+		public Vector3 Position;
+		public Vector2 Uv;
 	}
 }
